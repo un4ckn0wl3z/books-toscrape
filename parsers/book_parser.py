952478e841e373574d3d8f8ae56c804681883cc4
un4ckn0wl3z <slickerth@gmail.com>
2019-11-23 20:43:25 +0700

    add book parser

--- parsers/book_parser.py
+++ parsers/book_parser.py
@@ -1,29 +1,28 @@
-from bs4 import BeautifulSoup
 import re
 
 from locators.book_locators import BookLocators
 
 
 class BookParser:
-    def __init__(self, page):
-        self.soup = BeautifulSoup(page, 'html.parser')
+    def __init__(self, parent):
+        self.parent = parent
 
     @property
     def name(self):
         locator = BookLocators.NAME_LOCATOR
-        item_link = self.soup.select_one(locator)
+        item_link = self.parent.select_one(locator)
         item_name = item_link.attrs['title']
         return item_name
 
     @property
     def link(self):
         locator = BookLocators.LINK_LOCATOR
-        item_link = self.soup.select_one(locator).attrs['href']
+        item_link = self.parent.select_one(locator).attrs['href']
 
     @property
     def price(self):
         locator = BookLocators.PRICE_LOCATOR
-        item_price = self.soup.select_one(locator).string
+        item_price = self.parent.select_one(locator).string
 
         pattern = '£([0-9]+\.[0-9]+])'
         matcher = re.search(pattern, item_price)
@@ -32,7 +31,7 @@ class BookParser:
     @property
     def rating(self):
         locator = BookLocators.RATING_LOCATOR
-        star_rating_tag = self.soup.select_one(locator)
+        star_rating_tag = self.parent.select_one(locator)
         classes = star_rating_tag.attrs['class']
         rating_classes = [r for r in classes if r != 'star-rating']
         return rating_classes[0]
