c69396189b9f527c5132652e1ab67967603e2bda
un4ckn0wl3z <slickerth@gmail.com>
2019-11-30 21:02:20 +0700

    turn scraper to async

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import requests
 import logging
+import aiohttp
+import async_timeout
+import asyncio
+import time
 
 from pages.all_books_page import AllBooksPage
 
@@ -14,11 +18,35 @@ logger.info('Loading books list...')
 
 page_content = requests.get('http://books.toscrape.com/').content
 page = AllBooksPage(page_content)
+loop = asyncio.get_event_loop()
+
 books = page.books
 
-for page_num in range(1, page.page_count):
-    url = f'http://books.toscrape.com/catalogue/page-{page_num + 1}.html'
-    page_content = requests.get(url).content
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(f'Page took {time.time() - page_start}')
+            return await response.text()
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+urls = [f'http://books.toscrape.com/catalogue/page-{page_num + 1}.html' for page_num in range(1, page.page_count)]
+
+start = time.time()
+pages = loop.run_until_complete(get_multiple_pages(loop, *urls))
+print(f'All took {time.time() - start}')
+
+for page_content in pages:
     logger.debug('Creating AllBooksPage from page content.')
     page = AllBooksPage(page_content)
     books.extend(page.books)
---
+++ async_request.py
@@ -0,0 +1,28 @@
+import aiohttp
+import async_timeout
+import asyncio
+import time
+
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(f'Page took {time.time() - page_start}')
+            return response.status
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+loop = asyncio.get_event_loop()
+urls = ['http://www.chauat.ac.th/' for i in range(1000)]
+start = time.time()
+loop.run_until_complete(get_multiple_pages(loop, *urls))
+print(f'All took {time.time() - start}')
